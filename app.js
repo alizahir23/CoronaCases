b3201507bbddf6d8e69555a16fb16678c76fe709
alizahir23 <alizahiruddin@gmail.com>
2020-03-30 02:03:14 +0530

    added your location on top

--- app.js
+++ app.js
@@ -1,30 +1,89 @@
-const filter = document.querySelector("#filter");
-
 var requestOptions = {
     method: 'GET',
     redirect: 'follow'
 };
-let output = "";
-
+var output = "";
+let lat, long, currentCountry;
 const tableBody = document.querySelector('.tbody');
+const tableBodyCurrent = document.querySelector('.current');
 
 fetch("https://corona.lmao.ninja/countries?sort=country", requestOptions)
     .then(response => response.json())
     .then(result => {
-        result.sort((a, b) => parseFloat(a.cases) - parseFloat(b.cases));
-        result.reverse();
-        result.forEach((data, index) => {
 
 
-            output += `<tr>
-                        <th class ="removeMob">${index + 1}</th>
-                        <td class='country'>${data.country}</td>
-                        <td>${data.cases}</td>
-                        <td>${data.deaths}</td>
-                       </tr>`;
+
+        // Your Location
+
+
+
+        navigator.geolocation.getCurrentPosition(position => {
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
+
+            // fetching location
+
+            fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${long}&localityLanguage=en`)
+                .then(response => {
+
+                    console.log("location fetched");
+                    return response.json();
+
+                })
+                .then(data => {
+
+                    currentCountry = data.countryName;
+                    return currentCountry;
+
+                })
+                .then(countryName => {
+
+                    result.forEach((data, index) => {
+                        if (data.country == countryName) {
+
+                            output = `<tr class="current">
+                                        <th>${index + 1}</th>
+                                        <td class='country '>${data.country} <span class="badge badge-pill badge-warning">Your Location</span></td>
+                                        <td>${data.cases}</td>
+                                        <td>${data.deaths}</td>
+                                    </tr>`;
+
+                            tableBody.innerHTML = output;
+
+                        }
+                    })
+
+                    return result;
+
+                })
+                .then(result => {
+
+
+
+                    console.log(result)
+
+                    result.sort((a, b) => parseFloat(a.cases) - parseFloat(b.cases));
+                    result.reverse();
+                    result.forEach((data, index) => {
+
+
+                        output += `<tr>
+                                    <th>${index + 1}</th>
+                                    <td class='country'>${data.country}</td>
+                                    <td>${data.cases}</td>
+                                    <td>${data.deaths}</td>
+                                   </tr>`;
+                    })
+                    tableBody.innerHTML = output;
+                })
+
+
         })
-        tableBody.innerHTML = output;
+
+
+
     })
+
     .then(myInput.addEventListener('keyup', filterCountries))
     .catch(error => console.log('error', error));
 
